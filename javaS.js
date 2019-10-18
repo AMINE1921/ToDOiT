f074b5226904be69d0518b95cf2c04be1566ac12
AMINE1921 <amineoubaya1921@gmail.com>
2019-10-18 10:22:58 +0200

    Suppression des commentaires

--- javaS.js
+++ javaS.js
@@ -1,38 +1,3 @@
-// // Create a "close" button and append it to each list item
-// var myNodelist = document.querySelectorAll("ul#items li");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-// // Click on a close button to hide the current list item
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
-
-// var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-
-
 $(function () {
     var $list, $li, $newItemForm, $div;
     $list = $('ul#items');
@@ -40,7 +5,6 @@ $(function () {
     $li = $('ul#items li');
 
     $li.append('<span class="close">\u00D7</span>');
-    // $li.append('<span class="close"><img src="ToDOiT/Mockup/delete-2.png" alt="Supp"></span>');
 
     $('span').click(function () {
         $(this).parent().hide();
